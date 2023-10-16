074cd13973d25d7fd4e5eaf7fad9bafc0029884e
onehowon <flash0221@ajou.ac.kr>
2023-10-16 17:05:16 +0900

    test code

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -5,7 +5,7 @@ import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy{
 
-    private int discountPercent = 10
+    private int discountPercent = 10;
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP){
