bcbc67491814c3aba8ae0f9c48f470e1c5b58711
onehowon <flash0221@ajou.ac.kr>
2023-10-17 10:27:19 +0900

    Spring

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,20 +3,23 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
-
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    OrderService orderService = appConfig.orderService();
-
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl(null);
-        OrderService orderService = new OrderServiceImpl(null, null);
+
+    // AppConfig appConfig = new AppConfig();
+    // MemberService memberService = appConfig.memberService();
+    // OrderService orderService = appConfig.orderService();
+        // MemberService memberService = new MemberServiceImpl(null);
+        // OrderService orderService = new OrderServiceImpl(null, null);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
