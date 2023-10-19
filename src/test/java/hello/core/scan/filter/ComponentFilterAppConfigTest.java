1536e224a05069a374b917487cd9c2afac19245f
onehowon <flash0221@ajou.ac.kr>
2023-10-19 15:29:22 +0900

    componentScan

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -2,11 +2,11 @@ package hello.core.scan.filter;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
 public class ComponentFilterAppConfigTest {
 
@@ -16,13 +16,16 @@ public class ComponentFilterAppConfigTest {
         BeanA beanA = ac.getBean("beanA", BeanA.class);
         Assertions.assertThat(beanA).isNotNull();
 
-        ac.getBean("beanB", BeanB.class);
+        org.junit.jupiter.api.Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class)
+        );
     }
 
 
     @Configuration
-    @ComponentScan(includeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = MyIncludeComponent.class),
-    excludeFilters = @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    @ComponentScan(includeFilters = @ComponentScan.Filter(classes = MyIncludeComponent.class),
+    excludeFilters = @ComponentScan.Filter(classes = MyExcludeComponent.class)
     )
     static class ComponentFilterAppConfig{
     }
